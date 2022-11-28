eac4e1b339f87ea84c5bc033ba03587d765224ba
中山功太 <nakayamakouta@nakayamakoutanoMacBook-Pro-2.local>
2022-11-29 00:33:27 +0900

    モデルの追加

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 setup(
     name="liat_ml_roberta",
-    version="1.0.0",
+    version="1.1.0",
     description="Multi-Launguage RoBERTa trained by RIKEN-AIP LIAT.",
     long_description=readme,
     long_description_content_type="text/markdown",
--- src/liat_ml_roberta/main.py
+++ src/liat_ml_roberta/main.py
@@ -5,10 +5,12 @@ from transformers import RobertaModel, RobertaConfig
 from .tokenizers import select_tokenizer
 from .utils.data import load_json
 
+
 class RoBERTaTokenizer:
     @staticmethod
     def from_pretrained(model_name):
         return select_tokenizer(model_name)
+
 
 class RoBERTaModel:
     @staticmethod
@@ -16,6 +18,7 @@ class RoBERTaModel:
         meta_config_path = os.path.join(os.path.dirname(__file__), "data", model_name, "config.json")
         meta_config = load_json(meta_config_path)
         return RobertaModel.from_pretrained(meta_config["transformers_name"], config=config)
+
 
 class RoBERTaConfig:
     @staticmethod
